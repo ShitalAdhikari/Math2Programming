f91cc2701efeae453a1e0234755450d1e1925c1f
ShitalAdhikari <pppeterparker25@gmail.com>
2020-09-08 19:01:55 +0545

    outline for surface normal

---
+++ Scripts/SurfaceNormal.py
@@ -0,0 +1,18 @@
+'''
+    Here we are trying to predict the surface normal of the given figure.
+'''
+
+def findSurfaceNormal():
+    # TODO: Find the surface normal at given points
+    return None
+
+def gradientOfSurface():
+    # TODO: Find the gradient of the given surface
+    return None
+
+def draw_surface():
+    # TODO: Implementing the processing of drawing the surface
+    return None
+
+if __name__=="__main__":
+    print("hello world")
